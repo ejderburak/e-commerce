28ed2536e199f9fa289fd69b165f46c0025b01d1
Burak Ejder <147921910+ejderburak@users.noreply.github.com>
2025-08-08 09:35:17 +0300

    mappig

--- src/main/java/com/burakejder/controller/ProductController.java
+++ src/main/java/com/burakejder/controller/ProductController.java
@@ -1,17 +1,38 @@
 package com.burakejder.controller;
 
+import com.burakejder.DTO.DtoProduct;
 import com.burakejder.service.ProductService;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @RestController
-@RequestMapping("/rest/api/product")
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     private final ProductService productService;
+
+    @GetMapping
+    public List<DtoProduct> findAll(){
+        return productService.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public DtoProduct findById(@PathVariable Long id){
+        return productService.findById(id);
+    }
+
+    @PostMapping
+    public DtoProduct addProduct(@RequestBody DtoProduct dtoProduct){
+        return productService.addProduct(dtoProduct);
+    }
 
 }
 
--- src/main/java/com/burakejder/service/ProductService.java
+++ src/main/java/com/burakejder/service/ProductService.java
@@ -1,9 +1,16 @@
 package com.burakejder.service;
 
+import com.burakejder.DTO.DtoProduct;
+import com.burakejder.entities.Category;
+import com.burakejder.entities.Product;
+import com.burakejder.mapper.DtoMapper;
 import com.burakejder.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Data
 @AllArgsConstructor
@@ -12,4 +19,39 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    public List<DtoProduct> findAll() {
+
+        // getting all products from database
+        List<Product> productList = productRepository.findAll();
+
+        List<DtoProduct> dtoList = new ArrayList<>();
+
+        for(Product product : productList){
+            DtoProduct dtoProduct = DtoMapper.toDto(product); // turning into dtoProduct
+            dtoList.add(dtoProduct);
+        }
+        return dtoList;
+    }
+
+    public DtoProduct findById(Long id){
+        Optional<Product> optional = productRepository.findById(id);
+
+        if(optional.isEmpty()){ return null; }
+
+        return DtoMapper.toDto(optional.get());
+    }
+
+    public DtoProduct addProduct(DtoProduct dtoProduct){
+
+        //first convert dto ->  entity
+        Product product = DtoMapper.toEntity(dtoProduct);
+
+        // saving to db
+        Product dbProduct = productRepository.save(product);
+
+        // returning the saved proudct to dto
+        return DtoMapper.toDto(dbProduct);
+
+
+    }
 }
